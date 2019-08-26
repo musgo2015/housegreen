de3d1da72e4c4b500184cba352fa6bd80c710bcc
Carlos Rios Skinner <cskinner@ue.edu.co>
2019-08-26 14:48:11 -0500

    subir nevos archivos

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_QO9Rb3VaW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6l9ml9n6rjc8skuh12rrtf8ln7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://w263d2hsc3.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
